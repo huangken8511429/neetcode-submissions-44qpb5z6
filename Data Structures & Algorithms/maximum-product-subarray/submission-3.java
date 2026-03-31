a1d5e3cf98e26303b7b1d40899eaf4c98d637bbe
huangken8511429 <104516884+huangken8511429@users.noreply.github.com>
2026-04-01 00:58:30 +0800

    Add: maximum-product-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int ans = Integer.MIN_VALUE; // 注意答案可能是负数
+        int fMax = 1;
+        int fMin = 1;
+
+        // f[i] = Math.max(f[i-1] * now, now)
+        for (int i = 0 ;  i < nums.length; i++) {
+            int now = nums[i];
+            int prevFMax = fMax;
+            fMax = Math.max(Math.max(fMax * now, fMin * now), now);
+            // 因為fMax已經更新過了，所以要用之前的fMax
+            fMin = Math.min(Math.min(prevFMax * now, fMin * now), now);
+            ans = Math.max(ans, fMax);
+        }
+        return ans;
+    }
+}
