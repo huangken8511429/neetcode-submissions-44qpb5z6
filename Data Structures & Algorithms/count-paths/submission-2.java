a1e9e2354e24f0588594cf1b7150f333f3b6e81e
huangken8511429 <104516884+huangken8511429@users.noreply.github.com>
2026-04-20 22:54:57 +0800

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    // f[row][col] = f[row+1][col] + f[row][col+1]
+     public int uniquePaths(int m, int n) {
+        int[][] grid = new int[m + 1][n + 1];
+
+        for (int row = 0; row < m; row++) {
+            grid[row][0] = 1;
+        }
+        for (int col = 0; col < n; col++) {
+            grid[0][col] = 1;
+        }
+
+        for (int row = 1; row < m; row++) {
+          for (int col = 1; col < n; col++) {
+            grid[row][col] = grid[row-1][col] + grid[row][col - 1];
+         }
+       }
+      return grid[m-1][n-1];
+    }
+}
