c9792c9343e5f12db2a22a87f467d0f86bd31aaf
huangken8511429 <104516884+huangken8511429@users.noreply.github.com>
2026-04-23 18:30:25 +0800

    Add: buy-and-sell-crypto-with-cooldown - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] memo = new int[n][2];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+        return dfs(0, true, prices, memo);
+    }
+    
+    private int dfs(int i, boolean buying, int[] prices, int[][] memo) {
+        int n = prices.length;
+        if (i >= n) {
+            return 0;
+        }
+        int state = buying ? 1 : 0;
+        if (memo[i][state] != -1) {
+            return memo[i][state];
+        }
+        int cool = dfs(i+1, buying, prices, memo); // 什麼都不做的成本
+
+        if (buying) {
+            int buy = dfs(i+1, false, prices, memo) - prices[i]; // 未來收入 - 今天買的錢
+            return memo[i][state] = Math.max(buy, cool);
+        } else {
+            int sell = dfs(i+2, true, prices, memo) + prices[i]; // 未來收入 + 付費的錢 記得休市所以會是i+2
+            return memo[i][state] = Math.max(sell, cool);
+        }
+    }
+}
