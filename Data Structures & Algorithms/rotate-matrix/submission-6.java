fe57b1fdc1d164bf9cf3e3a29cda7244a33733b4
huangken8511429 <104516884+huangken8511429@users.noreply.github.com>
2026-05-23 23:22:33 +0800

    Add: rotate-matrix - submission-6

---
+++ Data Structures & Algorithms/rotate-matrix/submission-6.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        reverse(matrix);
+        int n = matrix.length;
+        for (int i = 0 ; i < n; i++) {
+            for (int j = i; j < matrix[i].length; j++) { //避免重複交換，所以j從i開始
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i]; // 沿著左上到右下的對角線，將對立的matrix做交換
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    private void reverse(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) { // 因為兩兩兌換，只要跑到一半就好，不然會交換兩次
+            int[] temp = matrix[i];
+            matrix[i] = matrix[n - 1 - i];
+            matrix[n - 1 - i] = temp;
+        }
+    }
+}
