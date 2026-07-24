e345000b489a57551bd75d53f2476424638fa29b
huangken8511429 <104516884+huangken8511429@users.noreply.github.com>
2026-07-24 18:02:31 +0800

    Add: word-break - submission-6

---
+++ Data Structures & Algorithms/word-break/submission-6.py
@@ -0,0 +1,20 @@
+from functools import cache
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordSet = set(wordDict)
+
+        @cache
+        def dfs(i):
+            if i < 0:
+                return True
+
+            for j in range(i, -1, -1):
+                if s[j:i+1] in wordSet:
+                    if dfs(j-1):
+                        return True
+
+            return False
+
+        return dfs(len(s) - 1)                
