5785c3d001238f0a6372050c41bd399949d14252
huangken8511429 <104516884+huangken8511429@users.noreply.github.com>
2026-08-04 16:37:15 +0800

    Add: interleaving-string - submission-8

---
+++ Data Structures & Algorithms/interleaving-string/submission-8.py
@@ -0,0 +1,20 @@
+from functools import cache
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        n1, n2 = len(s1), len(s2)
+        if n1 + n2 != len(s3):
+            return False
+
+        @cache
+        def dfs(i, j):
+            if i < 0 and j < 0:
+                return True
+            k = i + j + 1
+            if i >= 0 and s1[i] == s3[k] and dfs(i-1, j):
+                return True
+            if j >= 0 and s2[j] == s3[k] and dfs(i, j-1):
+                return True 
+            return False
+        return dfs(n1-1, n2-1)        
+
+                
