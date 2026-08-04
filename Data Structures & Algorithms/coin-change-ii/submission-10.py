b1aef09c58df904a7c83ef88b81a08361484ff7a
huangken8511429 <104516884+huangken8511429@users.noreply.github.com>
2026-08-04 15:22:10 +0800

    Add: coin-change-ii - submission-10

---
+++ Data Structures & Algorithms/coin-change-ii/submission-10.py
@@ -0,0 +1,11 @@
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        n = len(coins)
+        f = [0] * (amount + 1)
+        f[0] = 1
+
+        for x in coins:
+            for c in range(x, amount+1):
+                f[c] += f[c - x]
+        return f[amount]         
