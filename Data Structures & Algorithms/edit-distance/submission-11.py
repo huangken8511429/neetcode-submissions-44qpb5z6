f4d68e71f5aa102d5e395d54fd5c8bc5f6c94255
huangken8511429 <104516884+huangken8511429@users.noreply.github.com>
2026-08-07 16:29:40 +0800

    Add: edit-distance - submission-11

---
+++ Data Structures & Algorithms/edit-distance/submission-11.py
@@ -0,0 +1,14 @@
+from functools import cache
+class Solution:
+    def minDistance(self, s: str, t: str) -> int:
+        n, m = len(s), len(t)
+        @cache  
+        def dfs(i: int, j: int) -> int:
+            if i < 0:
+                return j+1
+            if j < 0:
+                return i+1
+            if s[i] == t[j]:
+                return dfs(i-1, j-1)
+            return min(dfs(i-1, j), dfs(i, j-1), dfs(i-1, j-1)) + 1            
+        return dfs(n - 1, m - 1)
